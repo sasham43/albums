0d5712468be2161bb2ea9895e519369f507c8cd8
sasham43 <sashasemail@gmail.com>
2018-07-01 18:16:52 -0500

    saving user

--- src/users.js
+++ src/users.js
@@ -5,7 +5,28 @@ router.get('/', (req, res, next)=>{
     req.spotify.getMe()
       .then(function(data) {
         console.log('Some information about the authenticated user', data.body);
-        res.send(data);
+        req.db.users.find({
+            spotify_user_id: data.body.id
+        }).then((results)=>{
+            console.log('user', results);
+            var new_user = {
+                display_name: data.body.display_name,
+                image: data.body.images[0].url,
+                uri: data.body.uri,
+                spotify_user_id: data.body.id
+            };
+            if(results.length > 0){
+                new_user.id = results[0].id;
+            }
+
+            req.db.users.save(new_user).then((results)=>{
+                res.send(results);
+            }).catch(next);
+
+            // res.send(user);
+        }).catch(next);
+
+        // res.send(data);
       }, function(err) {
         console.log('Something went wrong!', err);
         next(err);
