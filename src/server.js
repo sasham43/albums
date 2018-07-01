4bae7477296d588b06bbf4d64fecb26e03d30701
sasham43 <sashasemail@gmail.com>
2018-07-01 17:37:53 -0500

    getting spotify tokens

--- src/server.js
+++ src/server.js
@@ -4,8 +4,22 @@ var bodyParser = require('body-parser');
 var config = require('./config').get();
 
 var root = require('./root');
-// var works = require('./works');
-// var sentiment = require('./sentiment');
+var Spotify = require('spotify-web-api-node');
+var fs = require('fs');
+
+var scopes = ['user-read-private', 'user-read-email', 'user-library-read', 'user-top-read', 'user-read-recently-played', 'user-read-currently-playing', 'user-modify-playback-state', 'user-read-playback-state'],
+    redirectUri = 'http://localhost:3009/callback',
+    clientId = process.env.SPOTIFY_ID,
+    clientSecret = process.env.SPOTIFY_SECRET,
+    state = 'albums';
+
+    // console.log('s', process.env, process.env.SPOTIFY_SECRET);
+
+var spotify = new Spotify({
+    redirectUri: redirectUri,
+    clientId: clientId,
+    clientSecret: clientSecret
+});
 
 var dbconn = require('./db');
 dbconn('album-db');
@@ -22,7 +36,7 @@ app.use(bodyParser.json({
 
 // require('./auth.js')(app, config);
 
-var listenPort = process.env.PORT || 3001;
+var listenPort = process.env.PORT || 3009;
 
 app.use('/api',function(req, res, next){
   console.log('get url', req.url)
@@ -30,6 +44,34 @@ app.use('/api',function(req, res, next){
     req.db = db;
     next();
   });
+});
+app.get('/callback', function(req, res, next){
+    console.log('query', req.query);
+    spotify.authorizationCodeGrant(req.query.code)
+    .then(function(data) {
+      console.log('The token expires in ' + data.body['expires_in']);
+      console.log('The access token is ' + data.body['access_token']);
+      console.log('The refresh token is ' + data.body['refresh_token']);
+      refresh_token = data.body['refresh_token'];
+
+      // Set the access token on the API object to use it in later calls
+      spotify.setAccessToken(data.body['access_token']);
+      spotify.setRefreshToken(data.body['refresh_token']);
+
+      fs.writeFile('token.txt', refresh_token)
+      // getTracks();
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
+    // get the things
+    res.sendFile(__dirname + '/root/public/albums.html');
+});
+app.get('/auth', function(req, res, next){
+    var authorizeURL = spotify.createAuthorizeURL(scopes, state);
+
+    console.log(authorizeURL);
+
+    res.send(authorizeURL);
 });
 
 // app.use('/api/sentiment',sentiment);
