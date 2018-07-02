2e9e063f64519e5d5b0a507e840dd586cb4e39f8
sasham43 <sashasemail@gmail.com>
2018-07-01 19:08:55 -0500

    checking albums

--- src/albums.js
+++ src/albums.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var router = express.Router();
-
+const q = require('q');
 
 router.get('/update/:user_id', (req, res, next)=>{
     // req.spotify.getMySavedAlbums({
@@ -15,7 +15,8 @@ router.get('/update/:user_id', (req, res, next)=>{
     //     console.log('Something went wrong!', err);
     //     next(err);
     // });
-    saveAlbums(0, req.spotify, req.db, req.params.user_id)
+    // saveAlbums(0, req.spotify, req.db, req.params.user_id)
+    updateAlbums(req.spotify, req.db, req.params.user_id)
     res.send({
         message: 'started'
     })
@@ -24,6 +25,44 @@ router.get('/update/:user_id', (req, res, next)=>{
 router.get('/', (req, res, next)=>{
 
 });
+
+function collectAlbumIDs(spotify, offset, albums, cb){
+    spotify.getMySavedAlbums({
+        limit: 50,
+        offset: offset
+    }).then((data)=>{
+        data.body.items.map((a)=>{
+            albums.push(a.album.id);
+        });
+
+        if(data.body.total == albums.length){
+            cb(albums)
+        } else {
+            var new_offset = data.body.offset + 50;
+            collectAlbumIDs(spotify, new_offset, albums, cb);
+        }
+    })
+}
+
+function updateAlbums(spotify, db, user_id) {
+    collectAlbumIDs(spotify, 0, [], (results)=>{
+        var spotify_albums = results;
+        db.get_album_ids([user_id]).then((resp)=>{
+            var db_albums = resp.map((a)=>{
+                return a.spotify_album_id;
+            });
+
+            var same = db_albums.equals(spotify_albums);
+            if(same){
+                console.log('same albums');
+            } else {
+                console.log('different albums', db_albums);
+                console.log('\n\n');
+                console.log('spotify albums', spotify_albums);
+            }
+        })
+    })
+}
 
 function saveAlbums(offset, spotify, db, user_id) {
     spotify.getMySavedAlbums({
@@ -55,6 +94,36 @@ function saveAlbums(offset, spotify, db, user_id) {
         next(err);
     });
 }
+
+// Warn if overriding existing method
+if(Array.prototype.equals)
+    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;
+        }
+        else if (this[i] != array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
 
 
 module.exports = router;
---
+++ src/db/get_album_ids.sql
@@ -0,0 +1,1 @@
+SELECT spotify_album_id FROM albums WHERE user_id = $1;
